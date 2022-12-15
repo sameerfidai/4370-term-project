a5a1bfb14e184b6505ecf729c77cedc36c37d6bf
Sameer Fidai <sameerfidai@Sameers-Laptop.attlocal.net>
2022-12-15 12:57:30 -0500

    added chart template and query 1

--- app.py
+++ app.py
@@ -1,18 +1,61 @@
 from flask import Flask, render_template
 import conLayer
 
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
 app = Flask(__name__)
+
+options = [
+    {'link': '/chart1', 'label': 'Query 1'},
+    {'link': '/chart1', 'label': 'Query 2'},
+    {'link': '/chart1', 'label': 'Query 3'},
+    {'link': '/chart1', 'label': 'Query 4'},
+    {'link': '/chart1', 'label': 'Query 5'},
+
+]
 
 
 @app.route("/")
-def homepage():
-    return render_template("index.html")
+def index():
+    return render_template('index.html', options=options)
 
 
 @app.route("/db")
 def db():
-    data = conLayer.dbConnection()
-    return "connected to database"
+
+    # need to handle the data here, making the assumption that
+    #[('Action,Adventure,Drama', '99644'), ('Comedy,Drama', '98906'), ('Biography,Comedy,Crime', '987857'), ('Drama', '98587'), ('Comedy', '98587')]
+    # first category is the genre of movie, we need to get Action and the revenue(last element)
+    data = conLayer.query1()
+    return data
+
+
+@app.route("/dash")
+def dash():
+    return render_template("notdash2.html")
+
+
+# query 1
+@app.route('/chart1')
+def chart1():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header = "Fruit in North America"
+    description = """
+    A academic study of the number of apples, oranges and bananas in the cities of
+    San Francisco and Montreal would probably not come up with this chart.
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, header=header, description=description, options=options)
 
 
 if __name__ == "__main__":
--- conLayer.py
+++ conLayer.py
@@ -10,19 +10,27 @@ def dbConnection():
     password = os.getenv('MYSQL_PASSWORD')
     db = os.getenv('MYSQL_DB')
 
-    try:
-        with connect(
-            host=host,
-            user=user,
-            password=password,
-            db=db
-        ) as connection:
-            print(connection)
-    except Error as e:
-        print(e)
+    connection = connect(
+        host=host,
+        user=user,
+        password=password,
+        db=db
+    )
+
     # app.config['MYSQL_USER'] = user
     # app.config['MYSQL_PASSWORD'] = password
     # app.config['MYSQL_DB'] = db
     # mysql=MySQL(app)
     # cur=mysql.connection.cursor()
     return connection
+
+
+def query1():
+    try:
+        connection = dbConnection()
+        cursor = connection.cursor()
+        cursor.callproc('Query1')
+        for i in cursor.stored_results():
+            return str(i.fetchall())
+    except Exception as e:
+        print(e)
