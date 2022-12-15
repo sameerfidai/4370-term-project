cef2d37231a41d840593176debeedfd010a591b2
Sameer Fidai <sameerfidai@Sameers-Laptop.attlocal.net>
2022-12-15 14:20:51 -0500

    query2

--- conLayer.py
+++ conLayer.py
@@ -25,11 +25,25 @@ def dbConnection():
     return connection
 
 
+# query 1
 def query1():
     try:
         connection = dbConnection()
         cursor = connection.cursor()
         cursor.callproc('Query1')
+        for i in cursor.stored_results():
+            return str(i.fetchall())
+    except Exception as e:
+        print(e)
+
+
+# query2 need to figure out how to send inputs from flask server and
+def query2(year1, year2):
+    try:
+        args = [year1, year2]
+        connection = dbConnection()
+        cursor = connection.cursor()
+        cursor.callproc('Query2', args)
         for i in cursor.stored_results():
             return str(i.fetchall())
     except Exception as e:
