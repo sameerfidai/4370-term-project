f3918d3a5daf9d6dd14a4005383a4741d2341331
Sameer Fidai <sameerfidai@Sameers-Laptop.attlocal.net>
2022-12-15 18:22:39 -0500

    added query 5

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ options = [
     {'link': '/chart2', 'label': 'Query 2'},
     {'link': '/chart3', 'label': 'Query 3'},
     {'link': '/chart4', 'label': 'Query 4'},
-    {'link': '/chart1', 'label': 'Query 5'},
+    {'link': '/chart5', 'label': 'Query 5'},
 
 ]
 
@@ -32,11 +32,6 @@ def db():
     # first category is the genre of movie, we need to get Action and the revenue(last element)
     data = conLayer.query1()
     return data
-
-
-@app.route("/dash")
-def dash():
-    return render_template("notdash2.html")
 
 
 # query 1
@@ -59,7 +54,7 @@ def chart1():
     return render_template('chart1.html', graphJSON=graphJSON, header=header, description=description, options=options)
 
 
-# get data for query 2
+# GET data for query 2
 @app.route('/chart2')
 def chart2():
     return render_template('chart2data.html', options=options)
@@ -71,10 +66,12 @@ def chart2_post():
     from_year = request.form['from_year']
     to_year = request.form['to_year']
     data = conLayer.query2(from_year, to_year)
-    return render_template('chart2.html', options=options, from_year=from_year, to_year=to_year, data=data)
+    header = "Query 2"
+    description = "Description."
+    return render_template('chart2.html', options=options, from_year=from_year, to_year=to_year, data=data, header=header, description=description)
 
 
-# get data for query 3
+# GET data for query 3
 @app.route('/chart3')
 def chart3():
     return render_template('chart3data.html', options=options)
@@ -85,11 +82,12 @@ def chart3():
 def chart3_post():
     director_name = request.form['director_name']
     data = conLayer.query3(director_name)
-    print(data)
-    return render_template('chart3.html', options=options, dir_name=director_name, data=data)
+    header = "Query 3"
+    description = "Description."
+    return render_template('chart3.html', options=options, dir_name=director_name, data=data, header=header, description=description)
 
 
-# get data for query 4
+# GET data for query 4
 @app.route('/chart4')
 def chart4():
     return render_template('chart3data.html', options=options)
@@ -100,7 +98,27 @@ def chart4():
 def chart4_post():
     director_name = request.form['director_name']
     data = conLayer.query4(director_name)
-    return render_template('chart3.html', options=options, dir_name=director_name,data=data)
+    header = "Query 4"
+    description = "Description."
+    return render_template('chart3.html', options=options, dir_name=director_name, data=data, header=header, description=description)
+
+
+# GET data for query 5
+@app.route('/chart5')
+def chart5():
+    return render_template('chart5data.html', options=options)
+
+
+# render query 5
+@app.route('/chart5', methods=['POST'])
+def chart5_post():
+    from_year = request.form['from_year']
+    to_year = request.form['to_year']
+    genre = request.form['genre'] + '%'
+    data = conLayer.query5(from_year, to_year, genre)
+    header = "Query 5"
+    description = "Description."
+    return render_template('chart5.html', options=options, from_year=from_year, to_year=to_year, genre=genre, data=data, header=header, description=description)
 
 
 if __name__ == "__main__":
